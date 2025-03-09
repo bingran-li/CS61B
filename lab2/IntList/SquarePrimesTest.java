2a0951bb8f28e062fcdd6ea386a2f7d0e3d48864
Bingran LI <noch_zarathustra@outlook.com>
2025-03-09 21:56:49 +0800

    lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,12 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimes2() {
+        IntList lst = IntList.of(2, 3 , 13);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9 -> 169", lst.toString());
+        assertTrue(changed);
+    }
 }
